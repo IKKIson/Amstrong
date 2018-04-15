1c21fc1ad6ef2934cacc481f6b33e6eea3159f85
Sagacity Jang <gkagm2@gmail.com>
2018-04-16 01:51:40 +0900

    테이블 형태 대입해봄

--- webSite/htdocs/progress/my_progress.php
+++ webSite/htdocs/progress/my_progress.php
@@ -17,18 +17,71 @@
 
     $conn = db_init();
 
+    // $sql = "select from ";
+    // $result = mysql_query($conn, $sql);
+    // if(!$result){
+    //     echo "error";
+    // } else {
 
+    // }
 
     ?>
 
-    <div>
-        <table class="table table-bordered">
+    <center>
+        
+        <table width="250" boarder="1" cellpadding="0" cellspacing="0">
+            <caption>사용자</caption>
+            <th>Category</th>
+            <td>사물</td>
+            <tr></tr>
+            <td>animal</td>
+
+            <tr>
+                <th>Chapter</th>
+                <td>1</td>    
+                <td>
+                this
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                            <span class="sr-only">80% Complete (danger)</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <td>animal</td>
+
+            <tr>
+                <th>Chapter</th>
+                <td>1</td>    
+                <td>
+                this
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                            <span class="sr-only">80% Complete (danger)</span>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        </table>
+
+        <table>
+
+            <tr>
+                <td>fucking</td>
+                <td>shit</td>
+            </tr>
+
+            
             <tr>
                 <th>Email</th>
                 <th>Chapter</th>
                 <th>Category</th>
                 <th>Card Id</th>
                 <th>Check</th>
+                <td>fucki</td>
+                <td>fucki</td>
+                <td>fucki</td>
+                <td>fucki</td>
             </tr>
             <tr>
                 <td>fuck iy</td>
@@ -37,7 +90,7 @@
             </tr>
 
         </table>
-    </div>
+    </center>
     <center>
 
     <?php
@@ -57,6 +110,8 @@
         $oldEmail;
         $emailCk = false;
         ##
+
+        include "./table_form.php";
 
         $result = mysqli_query($conn, $sql);
         $count = 1;
