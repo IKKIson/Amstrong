9c7b94222da9764c4d9d66433bb99119cb842f9b
Sagacity Jang <gkagm2@gmail.com>
2018-05-15 05:50:36 +0900

    unity 프로젝터의 MarkerBasedAR 디렉토리 안의  파일을 쉽게 수정하기 위해 만든 디렉토리

---
+++ MarkerBasedAR/Scripts/Utils/WebConnectionManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WebConnectionManager : MonoBehaviour {
+    
+    protected string url;
+
+    public static WebConnectionManager instance = null;
+    // Use this for initialization
+    void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        //소멸되지않게 하기
+        DontDestroyOnLoad(gameObject);
+        GetObjectDB();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //Object database 불러오기
+    public void GetObjectDB()
+    {
+        StartCoroutine(GetObjectDBCo());
+    }
+    IEnumerator GetObjectDBCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("objectDB", "objectDB");
+
+        url = "http://gkagm2.dothome.co.kr/connectProgram/cardDatabase/objectDB.php";
+        WWW webRequest = new WWW(url, form); // request 객체 생성후 웹사이트 로그인 url에 전송
+        yield return webRequest;
+        Debug.Log("데이터베이스 가져오기 예지 :" + webRequest.text);
+    }
+
+    //Animal databse 불러오기
+    //public void GetAnimalDB()
+    //{
+
+    //}
+}
---
+++ MarkerBasedAR/Scripts/WebCamTextureMarkerBasedARExample.cs
@@ -0,0 +1,453 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+#if UNITY_5_3 || UNITY_5_3_OR_NEWER
+using UnityEngine.SceneManagement;
+#endif
+using OpenCVForUnity;
+using OpenCVMarkerBasedAR;
+
+namespace MarkerBasedARExample
+{
+    /// <summary>
+    /// Web cam texture marker based AR example.
+    /// </summary>
+    [RequireComponent(typeof(WebCamTextureToMatHelper))]
+    public class WebCamTextureMarkerBasedARExample : MonoBehaviour
+    {
+        public GameObject Q;
+        public GameObject MyCanvas;
+        public UnityEngine.UI.Text ShowInfoText;
+
+        [Header("Marker 3D Text")]
+        //TODO: 마커 갯수마다 3D Text를 만들어줘서 넣어줘야되는 문제를 해결할 방법을 찾아봐야 함
+        public TextMesh showStudyCardInfoText0;
+        public TextMesh showStudyCardInfoText1;
+        public TextMesh showStudyCardInfoText2;
+
+
+        /// <summary>
+        /// The texture.
+        /// </summary>
+        Texture2D texture;
+
+        /// <summary>
+        /// The AR camera.
+        /// </summary>
+        public Camera ARCamera;
+
+        /// <summary>
+        /// The cam matrix.
+        /// </summary>
+        Mat camMatrix;
+
+        /// <summary>
+        /// The dist coeffs.
+        /// </summary>
+        MatOfDouble distCoeffs;
+
+        /// <summary>
+        /// The marker detector.
+        /// </summary>
+        MarkerDetector markerDetector;
+
+        /// <summary>
+        /// The invert Y.
+        /// </summary>
+        Matrix4x4 invertYM;
+
+        /// <summary>
+        /// The transformation m.
+        /// </summary>
+        Matrix4x4 transformationM;
+
+        /// <summary>
+        /// The invert Z.
+        /// </summary>
+        Matrix4x4 invertZM;
+
+        /// <summary>
+        /// The ar m.
+        /// </summary>
+        Matrix4x4 ARM;
+        
+        /// <summary>
+        /// The marker settings.
+        /// </summary>
+        public MarkerSettings[] markerSettings;
+
+        /// <summary>
+        /// The should move AR camera.
+        /// </summary>
+        [Tooltip("If true, only the first element of markerSettings will be processed.")]
+        public bool
+            shouldMoveARCamera;
+
+        /// <summary>
+        /// The web cam texture to mat helper.
+        /// </summary>
+        WebCamTextureToMatHelper webCamTextureToMatHelper;
+        
+        // Use this for initialization
+        void Start ()
+        {
+            if (ScreenController.webcam == true)
+            {
+                MyCanvas.SetActive(true);
+                Q.SetActive(true);
+            }
+
+            webCamTextureToMatHelper = gameObject.GetComponent<WebCamTextureToMatHelper> ();
+            webCamTextureToMatHelper.Initialize ();
+
+
+        }
+
+        /// <summary>
+        /// Raises the web cam texture to mat helper inited event.
+        /// </summary>
+        public void OnWebCamTextureToMatHelperInited()
+        {
+            //Debug.Log("OnWebCamTextureToMatHelperInited");
+
+            Mat webCamTextureMat = webCamTextureToMatHelper.GetMat();
+
+            texture = new Texture2D(webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
+            gameObject.GetComponent<Renderer>().material.mainTexture = texture;
+
+
+            gameObject.transform.localScale = new Vector3(webCamTextureMat.cols(), webCamTextureMat.rows(), 1);
+
+            //Debug.Log("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
+
+
+            float width = webCamTextureMat.width();
+            float height = webCamTextureMat.height();
+
+            float imageSizeScale = 1.0f;
+            float widthScale = (float)Screen.width / width;
+            float heightScale = (float)Screen.height / height;
+            if (widthScale < heightScale)
+            {
+                Camera.main.orthographicSize = (width * (float)Screen.height / (float)Screen.width) / 2;
+                imageSizeScale = (float)Screen.height / (float)Screen.width;
+            }
+            else
+            {
+                Camera.main.orthographicSize = height / 2;
+            }
+
+
+            //set cameraparam
+            int max_d = (int)Mathf.Max(width, height);
+            double fx = max_d;
+            double fy = max_d;
+            double cx = width / 2.0f;
+            double cy = height / 2.0f;
+            camMatrix = new Mat(3, 3, CvType.CV_64FC1);
+            camMatrix.put(0, 0, fx);
+            camMatrix.put(0, 1, 0);
+            camMatrix.put(0, 2, cx);
+            camMatrix.put(1, 0, 0);
+            camMatrix.put(1, 1, fy);
+            camMatrix.put(1, 2, cy);
+            camMatrix.put(2, 0, 0);
+            camMatrix.put(2, 1, 0);
+            camMatrix.put(2, 2, 1.0f);
+            //Debug.Log("camMatrix " + camMatrix.dump());
+
+            distCoeffs = new MatOfDouble(0, 0, 0, 0);
+            //Debug.Log("distCoeffs " + distCoeffs.dump());
+
+            //calibration camera
+            Size imageSize = new Size(width * imageSizeScale, height * imageSizeScale);
+            double apertureWidth = 0;
+            double apertureHeight = 0;
+            double[] fovx = new double[1];
+            double[] fovy = new double[1];
+            double[] focalLength = new double[1];
+            Point principalPoint = new Point(0, 0);
+            double[] aspectratio = new double[1];
+
+
+            Calib3d.calibrationMatrixValues(camMatrix, imageSize, apertureWidth, apertureHeight, fovx, fovy, focalLength, principalPoint, aspectratio);
+
+            //Debug.Log("imageSize " + imageSize.ToString());
+            //Debug.Log("apertureWidth " + apertureWidth);
+            //Debug.Log("apertureHeight " + apertureHeight);
+            //Debug.Log("fovx " + fovx[0]);
+            //Debug.Log("fovy " + fovy[0]);
+            //Debug.Log("focalLength " + focalLength[0]);
+            //Debug.Log("principalPoint " + principalPoint.ToString());
+            //Debug.Log("aspectratio " + aspectratio[0]);
+
+
+            //To convert the difference of the FOV value of the OpenCV and Unity. 
+            double fovXScale = (2.0 * Mathf.Atan((float)(imageSize.width / (2.0 * fx)))) / (Mathf.Atan2((float)cx, (float)fx) + Mathf.Atan2((float)(imageSize.width - cx), (float)fx));
+            double fovYScale = (2.0 * Mathf.Atan((float)(imageSize.height / (2.0 * fy)))) / (Mathf.Atan2((float)cy, (float)fy) + Mathf.Atan2((float)(imageSize.height - cy), (float)fy));
+
+            //Debug.Log("fovXScale " + fovXScale);
+            //Debug.Log("fovYScale " + fovYScale);
+
+
+            //Adjust Unity Camera FOV https://github.com/opencv/opencv/commit/8ed1945ccd52501f5ab22bdec6aa1f91f1e2cfd4
+            if (widthScale < heightScale)
+            {
+                ARCamera.fieldOfView = (float)(fovx[0] * fovXScale);
+            }
+            else
+            {
+                ARCamera.fieldOfView = (float)(fovy[0] * fovYScale);
+            }
+
+
+
+            MarkerDesign[] markerDesigns = new MarkerDesign[markerSettings.Length];
+            //MarkerDesign[] markerDesigns = new MarkerDesign[3];
+            //Debug.Log("markerDesigns: " + markerDesigns.Length);
+
+
+            for (int i = 0; i < markerDesigns.Length; i++)
+            {
+                markerDesigns[i] = markerSettings[i].markerDesign;
+                //Debug.Log("markerDesigns count : : " + markerDesigns[i]);
+
+            }
+
+            markerDetector = new MarkerDetector(camMatrix, distCoeffs, markerDesigns);
+
+
+
+            invertYM = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, -1, 1));
+            //Debug.Log("invertYM " + invertYM.ToString());
+
+            invertZM = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, 1, -1));
+            //Debug.Log("invertZM " + invertZM.ToString());
+
+
+            //if WebCamera is frontFaceing,flip Mat.
+            if (webCamTextureToMatHelper.GetWebCamDevice().isFrontFacing)
+            {
+                webCamTextureToMatHelper.flipHorizontal = true;
+            }
+        }
+
+        /// <summary>
+        /// Raises the web cam texture to mat helper disposed event.
+        /// </summary>
+        public void OnWebCamTextureToMatHelperDisposed()
+        {
+            //Debug.Log("OnWebCamTextureToMatHelperDisposed");
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (webCamTextureToMatHelper.IsPlaying() && webCamTextureToMatHelper.DidUpdateThisFrame())
+            {
+
+                Mat rgbaMat = webCamTextureToMatHelper.GetMat();
+
+                markerDetector.processFrame(rgbaMat, 1);
+
+
+                foreach (MarkerSettings settings in markerSettings)
+                {
+                    settings.setAllARGameObjectsDisable();
+                }
+
+                if (shouldMoveARCamera)
+                {
+                    List<Marker> findMarkers = markerDetector.getFindMarkers();
+                    if (findMarkers.Count > 0)
+                    {
+                        //Debug.Log("I found marker : " + findMarkers.Count);
+                        Marker marker = findMarkers[0];
+
+                        if (markerSettings.Length > 0)
+                        {
+                            MarkerSettings settings = markerSettings[0];
+
+                            if (marker.id == settings.getMarkerId())
+                            {
+                                transformationM = marker.transformation;
+                                //Debug.Log("transformationM " + transformationM.ToString());
+                                CardInfo.CheckID(marker.id); //화면에 노출된 카드 체크하기
+                                ShowInfoText.text = CardInfo.PrintIsCardCheck(); // 카드 체크됬는지 확인하는 메세지 리턴 
+                                Debug.Log(CardInfo.PrintIsCardCheck());
+
+                                GameObject ARGameObject = settings.getARGameObject();
+                                if (ARGameObject != null)
+                                {
+                                    ARM = ARGameObject.transform.localToWorldMatrix * invertZM * transformationM.inverse * invertYM;
+                                    //Debug.Log ("arM " + arM.ToString ());
+                                    ARGameObject.SetActive(true);
+                                    ARUtils.SetTransformFromMatrix(ARCamera.transform, ref ARM);
+                                }
+
+                            }
+                        }
+                    }
+                    
+                }
+                else
+                {
+
+                    List<Marker> findMarkers = markerDetector.getFindMarkers();
+                    //Debug.Log("I found marker !: " + findMarkers.Count);
+                    for (int i = 0; i < findMarkers.Count; i++)
+                    {
+                        Marker marker = findMarkers[i];
+
+                        foreach (MarkerSettings settings in markerSettings)
+                        {
+                            if (marker.id == settings.getMarkerId())
+                            {
+                                //Debug.Log("이게 진짜다!!!!!!!!!!!marker id : " + marker.id);
+
+                                if (UserInfo.wordStudyObject) // 사물 공부
+                                {
+                                    CardInfo.CheckID(marker.id); //화면에 노출된 카드 체크하기
+                                    //ShowInfoText.text = CardInfo.PrintIsCardCheck(); // 카드 체크됬는지 확인하는 메세지 리턴 
+                                    //Debug.Log(CardInfo.PrintIsCardCheck());
+                                    int currentIndex = CardInfo.GetCurrentTrueIndex(); //화면에 보여주고있는 마커의 해당 index를 가져옴
+                                    //Debug.Log("currnetIndex. : " + currentIndex);
+                                    //임시 데이터베이스에서 가져온 정보를 3D text에 뿌려줌
+                                    if (currentIndex == 0)
+                                    {
+                                        // 인식 속도 때문에 함수 안만듬
+                                        showStudyCardInfoText0.text =
+                                        "[영어]" + ObjectDB.english[currentIndex] + "\n" +
+                                        "[한국어]" + ObjectDB.korean[currentIndex] + "\n" +
+                                        "[품사]" + ObjectDB.phoneticAlpabet[currentIndex] + "\n";
+                                }
+                                    else if (currentIndex == 1)
+                                    {
+                                        showStudyCardInfoText1.text =
+                                        "[영어]" + ObjectDB.english[currentIndex] + "\n" +
+                                        "[한국어]" + ObjectDB.korean[currentIndex] + "\n" +
+                                        "[품사]" + ObjectDB.phoneticAlpabet[currentIndex] + "\n";
+                                }
+                                    else if (currentIndex == 2)
+                                    {
+                                        showStudyCardInfoText2.text =
+                                        "[영어]" + ObjectDB.english[currentIndex] + "\n" +
+                                        "[한국어]" + ObjectDB.korean[currentIndex] + "\n" +
+                                        "[품사]" + ObjectDB.phoneticAlpabet[currentIndex] + "\n";
+                                    }
+                                    CardInfo.SetALLCardFlagsFalse(); //모든   체크를 false로 만들기
+                            }
+                            else if (UserInfo.wordStudyAnimal) //동물 공부
+                            {
+
+                            }
+                            else if (UserInfo.wordStudyNumber) //숫자 공부
+                            {
+
+                            }
+                            else if (UserInfo.wordStudyTime) //시간 공부
+                            {
+
+                            }
+                            else if (UserInfo.wordStudyHuman) // 인물 공부
+                            {
+
+                            }
+                            else if (UserInfo.menuWordMatching) //단어 매칭이
+                            {
+
+
+                            }
+                            else if (UserInfo.menuMakeSentence) //문장 만들기
+                            {
+
+                            }
+
+
+
+
+                            transformationM = marker.transformation;
+                                //Debug.Log ("transformationM " + transformationM.ToString ());
+
+                                ARM = ARCamera.transform.localToWorldMatrix * invertYM * transformationM * invertZM;
+                                //Debug.Log ("arM " + arM.ToString ());
+
+                                GameObject ARGameObject = settings.getARGameObject();
+                                if (ARGameObject != null)
+                                {
+
+                                    ARUtils.SetTransformFromMatrix(ARGameObject.transform, ref ARM);
+                                    ARGameObject.SetActive(true);
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                Utils.matToTexture2D(rgbaMat, texture, webCamTextureToMatHelper.GetBufferColors());
+            }
+
+        }
+
+        /// <summary>
+        /// Raises the disable event.
+        /// </summary>
+        void OnDisable ()
+        {
+            webCamTextureToMatHelper.Dispose ();
+        }
+        
+        /// <summary>
+        /// Raises the back button event.
+        /// </summary>
+        public void OnBackButton () //back button 클릭 시 MainUI scene으로 전환
+        {
+            MyCanvas.SetActive(false);
+            Q.SetActive(false);
+            ScreenController.menuUI = true;
+            ScreenController.loginUI = false;
+            ScreenController.webcam = false;
+            SceneManager.LoadScene ("MainUI");
+            
+        }
+
+        /// <summary>
+        /// Raises the play button event.
+        /// </summary>
+        public void OnPlayButton ()
+        {
+            webCamTextureToMatHelper.Play ();
+        }
+        
+        /// <summary>
+        /// Raises the pause button event.
+        /// </summary>
+        public void OnPauseButton ()
+        {
+            webCamTextureToMatHelper.Pause ();
+        }
+        
+        /// <summary>
+        /// Raises the stop button event.
+        /// </summary>
+        public void OnStopButton ()
+        {
+            webCamTextureToMatHelper.Stop ();
+        }
+        
+        /// <summary>
+        /// Raises the change camera button event.
+        /// </summary>
+        public void OnChangeCameraButton ()
+        {
+            webCamTextureToMatHelper.Initialize (null, webCamTextureToMatHelper.requestedWidth, webCamTextureToMatHelper.requestedHeight, !webCamTextureToMatHelper.requestedIsFrontFacing);
+        }
+
+    }
+    
+}
