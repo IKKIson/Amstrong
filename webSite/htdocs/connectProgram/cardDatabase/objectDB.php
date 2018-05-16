cab5254e8774287634be91089dc83a3dd2a84a66
Sagacity Jang <gkagm2@gmail.com>
2018-05-16 20:21:11 +0900

    카듭 불러오기 쿼리문작성

--- webSite/htdocs/connectProgram/cardDatabase/objectDB.php
+++ webSite/htdocs/connectProgram/cardDatabase/objectDB.php
@@ -1,8 +1,26 @@
 <?php
+    class ConnectApp{
+
+        function setSelect($select){
+            
+        }
+
+        function getDBQuery($select){
+            return "select * from $select";
+        }
+    }
+
+    //앱과 연결하는 클래스 객체 생성
+    $connApp = new ConnectApp;
+
+    //앱으로부터 받아옴
+    $select = $_POST['menu'];
+   
+    //해당되는 선택문을 선택하여 데이터베이스에서 가져온다
+    $sql = $connApp.getDBQuery($seslect);
+
     //import databaseManager class
     include_once($_SERVER['DOCUMENT_ROOT']."/model_database/util.php");
-
-
 
     $conn = db_init();
 
@@ -10,8 +28,6 @@
         die('Could not Connect:' . mysqli_error()); //The die() function prints a message and exits the current script.
     }
     
-    $sql = "select * from ObjectDB"; //ObjectDB에 있는 값들 불러오기
-
     $result = mysqli_query($conn,$sql);
 
     $string = '';
@@ -27,14 +43,7 @@
         // echo $row['relation_sentence_en'];
         // echo $row['relation_sentence_ko'];
         // echo '<br>';
-
     }
-
-    echo $string;
-    // die($string);
+    die($string);
     // die($row['english'] + ','+$row['korean'] + ','+$row['phonetic_alpabet'] + ','+$row['part_of_speech'] + ','+$row['relation_sentence_en'] + ','+$row['relation_sentence_ko']);
-    
-    
-    
-
 ?>
