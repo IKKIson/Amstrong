43698a6b197c9a6bcf6265e4b5789f760df070e5
HyeonMyeongJang <gkagm2@gmail.com>
2018-10-07 16:09:40 +0900

    update

--- MarkerBasedAR/Scripts/Utils/ScreenController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ScreenController{
-    public static bool loginUI = true;
-    public static bool menuUI =false;
-    public static bool webcam = false;
-}
